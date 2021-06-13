18fda255b9b2748aa1e01fd35690e541ab6d6213
ali almanea <79771322+zaabta@users.noreply.github.com>
2021-06-13 16:33:42 +0300

    Update bird.js

--- my flappy bird ai/bird.js
+++ my flappy bird ai/bird.js
@@ -16,8 +16,8 @@ class Bird {
     this.gravity = 0.7;
     this.lift = -12;
     this.velocity = 0;
-    this.width = 50;
-    this.height = 45;
+    this.width = 55;
+    this.height = 50;
     if(brain){
       this.brain = brain.copy();
     }else{
